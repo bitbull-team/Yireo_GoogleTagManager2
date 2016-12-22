e34efc7ebe9f5058c13645175e19bc5daa362a85
andra lungu <andra.lungu@bitbull.it>
2016-12-22 16:21:15 +0100

    extracted no script part to be inside body and the container

---
+++ Block/NoScript.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author Andra Lungu <andra.lungu@bitbull.it>
+ * Date: 22/12/16
+ * Time: 15.14
+ */
+
+
+namespace Yireo\GoogleTagManager2\Block;
+
+/**
+ * Class \Yireo\GoogleTagManager2\Block\NoScript
+ */
+class NoScript extends Generic{
+
+}
